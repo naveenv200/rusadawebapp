4182822c4ddc92f82aa0ebe01191330ec52e1c83
naveenv200 <42172006+naveenv200@users.noreply.github.com>
2021-07-26 12:59:37 +0530

    commit02

---
+++ RusadaAssignment/Custom Validation/LessDateAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RusadaAssignment.Custom_Validation
+{
+    public class LessDateAttribute:ValidationAttribute
+    {
+
+        public LessDateAttribute() : base("{0} Should Less than current Date")
+        {
+                
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime propValue = Convert.ToDateTime(value);
+            if (propValue <= DateTime.Now)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    
+}
--- RusadaAssignment/Models/RusadaWebApp.cs
+++ RusadaAssignment/Models/RusadaWebApp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using RusadaAssignment.Custom_Validation;
 
 namespace RusadaAssignment.Models
 {
@@ -10,18 +11,28 @@ namespace RusadaAssignment.Models
     {
         public int Id { get; set; }
 
+        //Make
+        [Required(ErrorMessage = "Field can not be empty!")]
         [MaxLength(128, ErrorMessage = "Maximum number of characters that can be entered is 128!")]
         public string Make { get; set; }
 
+        //Model
+        [Required(ErrorMessage = "Field can not be empty!")]
         [MaxLength(128, ErrorMessage = "Maximum number of characters that can be entered is 128!")]
         public string Model { get; set; }
 
+        //Registration
+        [Required(ErrorMessage = "Field can not be empty!")]
         public string Registration { get; set; }
 
+        //Location
+        [Required(ErrorMessage = "Field can not be empty!")]
         [MaxLength(225, ErrorMessage = "Maximum number of characters that can be entered is 225!")]
         public string Location { get; set; }
 
-        
+        //DateTime
+        [Required(ErrorMessage = "Field can not be empty!")]
+        [LessDate]
         public DateTime dateTime { get; set; }
 
         public RusadaWebApp()
